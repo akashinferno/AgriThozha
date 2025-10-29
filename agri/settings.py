bcdb999bf6248039545c618649ab25cf8f0cef0c
akashinferno <akashinferno007@gmail.com>
2025-10-29 15:46:45 +0530

    Added CORS headers and debug

--- agri/settings.py
+++ agri/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3z)d=ddd2zri8*v71fu)=q^*89!hb0)y80ul@=m5^wf@_zn05^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
